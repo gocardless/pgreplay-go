f5938d2fb83b54be21830b6d801dbe9d4f0cc07c
Lawrence Jones <lawrjone@gmail.com>
2019-02-25 09:32:37 +0000

    pkg/pgreplay/integration
    Add integration tests for the entire replay flow, covering the parsing
    of logs through to the result we expect in the database. This commit
    also fixes a couple of bugs that were surfaced by the addition of a
    proper test suite.
    
    One subtle but important bug was caused by a side effect of us
    incorrectly signalling that we'd parsed a DETAIL parameter log-line. As
    we returned nil for these entries, it was possible for subsequent
    DETAILs to override the query parameters of the previously unbound
    query, clobbering whatever had been bound. We now have a special
    BoundExecuteItem which solves this problem, but we should re-evaluate
    the current approch of modelling query types.

---
+++ pkg/pgreplay/integration/testdata/structure.sql
@@ -0,0 +1,89 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 10.5
+-- Dumped by pg_dump version 10.5
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: 
+--
+
+CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
+
+
+--
+-- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: 
+--
+
+COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
+
+
+SET default_tablespace = '';
+
+SET default_with_oids = false;
+
+--
+-- Name: logs; Type: TABLE; Schema: public; Owner: lawrence
+--
+
+CREATE TABLE public.logs (
+    id integer NOT NULL,
+    author text,
+    message text
+);
+
+
+ALTER TABLE public.logs OWNER TO lawrence;
+
+--
+-- Name: logs_id_seq; Type: SEQUENCE; Schema: public; Owner: lawrence
+--
+
+CREATE SEQUENCE public.logs_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.logs_id_seq OWNER TO lawrence;
+
+--
+-- Name: logs_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: lawrence
+--
+
+ALTER SEQUENCE public.logs_id_seq OWNED BY public.logs.id;
+
+
+--
+-- Name: logs id; Type: DEFAULT; Schema: public; Owner: lawrence
+--
+
+ALTER TABLE ONLY public.logs ALTER COLUMN id SET DEFAULT nextval('public.logs_id_seq'::regclass);
+
+
+--
+-- Name: logs logs_pkey; Type: CONSTRAINT; Schema: public; Owner: lawrence
+--
+
+ALTER TABLE ONLY public.logs
+    ADD CONSTRAINT logs_pkey PRIMARY KEY (id);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
